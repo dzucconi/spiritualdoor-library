aebe89ca4808de9e52978a76e2177ea434148fbd
dzucconi <mail@damonzucconi.com>
2016-08-17 16:27:37 +0200

    Generate library

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
+const fs = require('fs');
 const Ivona = require('ivona-node');
+const data = require('./data');
 
 const ivona = new Ivona({
   accessKey: process.env.IVONA_ACCESS_KEY,
   secretKey: process.env.IVONA_SECRET_KEY
 });
 
-console.log(process.env.IVONA_ACCESS_KEY)
+const list = () =>
+  ivona.listVoices()
+    .on('complete', function(voices) {
+      console.log(voices);
+    });
 
-ivona.listVoices()
-  .on('complete', function(voices) {
-    console.log(voices);
-  });
+const voices = {
+  female: { Gender: 'Female', Language: 'it-IT', Name: 'Carla' },
+  male: { Gender: 'Male', Language: 'it-IT', Name: 'Giorgio' }
+};
 
-//  ivona.createVoice(text, config)
-//  [string] text - the text to be spoken
-//  [object] config (optional) - override Ivona request via 'body' value
-// ivona.createVoice('This is the text that will be spoken.', {
-//   body: {
-//     voice: {
-//       name: 'Salli',
-//       language: 'en-US',
-//       gender: 'Female'
-//     }
-//   }
-// }).pipe(fs.createWriteStream('text.mp3'));
+const speak = (text, voice) =>
+  ivona
+    .createVoice(text, {
+      body: { voice: voice }
+    })
+    .pipe(fs.createWriteStream(`./output/${voice.Gender}/${text}.mp3`));
+
+const points = data.map(x => x.traditional_wind_point);
+
+points.map(point => speak(point, voices.male));
